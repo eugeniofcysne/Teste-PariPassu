63370c86b2e0dce71209eb8d715689be957549ec
Eugênio Fiorelli Cysne <42222815+eugeniofcysne@users.noreply.github.com>
2019-10-25 15:05:58 -0300

    Primeira Versão - FilmeDAOImpl em andamento
    Implementação da classe FilmeDAO em andamento.

---
+++ teste de estágio/src/dao/jdbc/FilmeDAOImpl.java
@@ -0,0 +1,108 @@
+package dao.jdbc;
+
+import dao.FilmeDAO;
+import entidades.Filme;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+
+
+public class FilmeDAOImpl implements FilmeDAO {
+
+	@Override
+	public void insert(Connection conn, Filme filme) throws Exception {
+		
+		//conversão da data
+		SimpleDateFormat in= new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat out = new SimpleDateFormat("dd/MM/yyyy");		 
+		String dataFormatada = out.format(in.parse(filme.getDataLancamento().toString()));
+		
+		//pega id filme
+		
+		Integer idFilme = this.getNextId(conn);
+		
+		//Construção da String 'sql' para realizar o insert
+		String sql = "INSERT INTO filme (id_filme,data_lancamento,nome,descricao) VALUES (";
+		sql = sql.concat(Integer.toString(idFilme));
+		sql = sql.concat(",'");
+		sql = sql.concat(dataFormatada);
+		sql = sql.concat("','");
+		sql = sql.concat(filme.getNome());
+		sql = sql.concat("','");	
+		sql = sql.concat(filme.getDescricao());
+		sql = sql.concat("');");
+		//Prepara a instrução SQL
+		PreparedStatement ps = conn.prepareStatement(sql);
+		//Executa a instrução SQL
+		ps.execute();
+        conn.commit();
+	}
+
+	@Override
+	public Integer getNextId(Connection conn) throws Exception {
+		String sql = "select nextval('seq_en_cliente')";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        return rs.getInt(1);
+
+	}
+
+	@Override
+	public void edit(Connection conn, Filme filme) throws Exception {
+		
+		//pegando o ID do filme a ser alterado
+		Integer idFilme = this.getNextId(conn); 
+		Integer decisao=idFilme;
+		
+		while (decisao>= idFilme) {
+			Scanner scanner = new Scanner(System.in);
+			String texto = "Qual a idFilme que deseja alterar? Deve estar entre 1 e ";
+			texto=texto.concat(Integer.toString(idFilme-1));
+			texto=texto.concat("\n");		
+			System.out.println(texto);
+			decisao = scanner.nextInt();
+		}
+		//pegando o novo nome
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Digite o nome desejado: \n");
+		String nomeDesejado = scanner.nextLine();
+		
+		//motando a string sql
+		String sql = "update en_filme set nome = ('";
+		sql = sql.concat(nomeDesejado);
+		sql = sql.concat("' where id_filme = ");	
+		sql = sql.concat(Integer.toString(idFilme));
+		
+		//fazendo o update
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.execute();
+        conn.commit();
+		
+	}
+
+	@Override
+	public void delete(Connection conn, Integer idFilme) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Filme find(Connection conn, Integer idFilme) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Filme> list(Connection conn) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
